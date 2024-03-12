59c4ab2d3248950ac7bf1638f62996f1768f8122
octavioserrago <octavioserrago.dev@gmail.com>
2024-03-12 15:10:28 -0300

    TEST01

--- src/main/java/com/verona/model/DatabaseConnection.java
+++ src/main/java/com/verona/model/DatabaseConnection.java
@@ -10,7 +10,7 @@ public class DatabaseConnection {
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/MarmoleriaVerona", "root", "");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/MarmoleriaVerona", "root", "1");
         } catch (Exception e) {
             System.err.println("Error al conectarse: " + e.getMessage());
         }
