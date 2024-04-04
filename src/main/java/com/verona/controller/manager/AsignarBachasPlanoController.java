d7692246332a9dbde9b10270bc60d96caa3f026d
octavioserrago <octavioserrago.dev@gmail.com>
2024-04-04 11:06:40 -0300

    feat: Add functionality to assign sinks to a blueprint
    - Updated AsignarBachasPlanoController to include functionality for assigning sinks to a blueprint.
    - Implemented descontarCantidad method in Sink model to decrease the quantity of a selected sink.
    - Added insertarSinkPlano method in Sink model to insert sink-blueprint associations into the database.
    - Updated btnCargarClicked method to correctly handle sink assignment to a blueprint.

--- src/main/java/com/verona/controller/manager/AsignarBachasPlanoController.java
+++ src/main/java/com/verona/controller/manager/AsignarBachasPlanoController.java
@@ -1,11 +1,17 @@
 package com.verona.controller.manager;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import com.verona.controller.SceneController;
+import com.verona.model.Bacha;
+import com.verona.model.Plano;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
@@ -24,7 +30,7 @@ public class AsignarBachasPlanoController {
     private Button btnVolver;
 
     @FXML
-    private ComboBox<?> comboboxBachas;
+    private ComboBox<Bacha> comboboxBachas;
 
     @FXML
     private Label labelIndicativo;
@@ -32,9 +38,23 @@ public class AsignarBachasPlanoController {
     @FXML
     private TextField textFieldCodigoPlano;
 
+    private int idPlano;
+
+    public int getIdPlano() {
+        return idPlano;
+    }
+
+    public void setIdPlano(int idPlano) {
+        this.idPlano = idPlano;
+    }
+
+    Plano plano = new Plano(null, null, null, null, 0, null, null);
+    Bacha bacha = new Bacha(0, null, null, 0);
+
     @FXML
-    void initialize() {
+    void initialize() throws SQLException {
         noVisibles();
+        cargarBachasEnComboBox();
     }
 
     public void noVisibles() {
@@ -45,12 +65,76 @@ public class AsignarBachasPlanoController {
 
     @FXML
     void btnBuscarClicked(ActionEvent event) {
+        String codigoPlano = textFieldCodigoPlano.getText();
+        if (!codigoPlano.isEmpty()) {
+            try {
+                int idPlano = plano.buscarPlano(codigoPlano);
 
+                if (idPlano > 0) {
+                    labelIndicativo.setVisible(true);
+                    comboboxBachas.setVisible(true);
+                    btnCargar.setVisible(true);
+                    setIdPlano(idPlano);
+                } else {
+
+                    labelIndicativo.setVisible(false);
+                    comboboxBachas.setVisible(false);
+                    btnCargar.setVisible(false);
+
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Plano no encontrado");
+                    alert.setHeaderText(null);
+                    alert.setContentText("El plano con el código especificado no se encontró.");
+                    alert.showAndWait();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Campo Vacío");
+            alert.setHeaderText(null);
+            alert.setContentText("Por favor complete el campo de código de plano.");
+            alert.showAndWait();
+        }
+    }
+
+    private void cargarBachasEnComboBox() throws SQLException {
+        List<Bacha> bachas = bacha.obtenerBachas();
+        ObservableList<Bacha> observableBachas = FXCollections.observableArrayList(bachas);
+        comboboxBachas.setItems(observableBachas);
     }
 
     @FXML
     void btnCargarClicked(ActionEvent event) {
+        Bacha bachaSeleccionada = comboboxBachas.getValue();
+        if (bachaSeleccionada != null) {
+            int cantidadADescontar = bachaSeleccionada.getCantidad() - 1;
+            try {
+                int idBacha = bachaSeleccionada.getBachasID();
+                bachaSeleccionada.descontarCantidad(idBacha, cantidadADescontar, getIdPlano());
 
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Operación Exitosa");
+                alert.setHeaderText(null);
+                alert.setContentText("Bacha asignada correctamente al plano.");
+                alert.showAndWait();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Hubo un error al asignar la bacha al plano.");
+                alert.showAndWait();
+            }
+        } else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Bacha no seleccionada");
+            alert.setHeaderText(null);
+            alert.setContentText("Por favor seleccione una bacha.");
+            alert.showAndWait();
+        }
     }
 
     @FXML
--- src/main/java/com/verona/model/Bacha.java
+++ src/main/java/com/verona/model/Bacha.java
@@ -15,6 +15,7 @@ public class Bacha {
     private String nombreModelo;
     private String medidas;
     private int cantidad;
+    public String tipoBacha;
 
     public Bacha(int marcasBachasID, String nombreModelo, String medidas, int cantidad) {
         this.marcasBachasID = marcasBachasID;
@@ -71,9 +72,18 @@ public class Bacha {
         this.cantidad = cantidad;
     }
 
+    public String getTipoBacha() {
+        return tipoBacha;
+    }
+
+    public void setTipoBacha(String tipoBacha) {
+        this.tipoBacha = tipoBacha;
+    }
+
     @Override
     public String toString() {
-        return nombreMarca + " - " + nombreModelo + " - " + medidas;
+        return nombreMarca + " - " + nombreModelo + " - Tipo: " + tipoBacha + "- Medidas:" + medidas + "- Cantidad: "
+                + cantidad;
     }
 
     DatabaseConnection con = new DatabaseConnection();
@@ -81,6 +91,22 @@ public class Bacha {
     Connection conexion = con.conectar();
 
     PreparedStatement stmt;
+
+    public void descontarCantidad(int bachasID, int cantidadADescontar, int idPlano) throws SQLException {
+        String sqlUpdate = "UPDATE Bachas SET cantidad = ? WHERE bachasID = ?";
+        try (PreparedStatement pstmt = conexion.prepareStatement(sqlUpdate)) {
+            pstmt.setInt(1, cantidadADescontar);
+            pstmt.setInt(2, bachasID);
+            pstmt.executeUpdate();
+        }
+
+        String sqlInsert = "INSERT INTO BachaPlanos (bachasID, planosID) VALUES (?, ?)";
+        try (PreparedStatement pstmt = conexion.prepareStatement(sqlInsert)) {
+            pstmt.setInt(1, bachasID);
+            pstmt.setInt(2, idPlano);
+            pstmt.executeUpdate();
+        }
+    }
 
     public List<Bacha> obtenerBachasStock() throws SQLException {
         String sql = "SELECT marcasBachasID, nombreModelo, medidas, cantidad FROM Bachas WHERE marcasBachasID = 2";
@@ -108,9 +134,11 @@ public class Bacha {
     }
 
     public List<Bacha> obtenerBachas() throws SQLException {
-        String sql = "SELECT b.marcasBachasID, b.nombreModelo, b.medidas, b.cantidad, m.nombreMarca " +
+        String sql = "SELECT b.bachasID, b.marcasBachasID, b.nombreModelo, b.tipoBacha, b.medidas, b.cantidad, m.marca "
+                +
                 "FROM Bachas b " +
-                "JOIN MarcasBachas m ON b.marcasBachasID = m.marcasBachasID";
+                "JOIN MarcasBachas m ON b.marcasBachasID = m.marcaBachaID";
+
         List<Bacha> bachasList = new ArrayList<>();
 
         try {
@@ -118,19 +146,28 @@ public class Bacha {
             ResultSet resultSet = stmt.executeQuery();
 
             while (resultSet.next()) {
+                int bachasID = resultSet.getInt("bachasID");
                 int marcasBachasID = resultSet.getInt("marcasBachasID");
                 String nombreModelo = resultSet.getString("nombreModelo");
+                String tipoBacha = resultSet.getString("tipoBacha");
                 String medidas = resultSet.getString("medidas");
                 int cantidad = resultSet.getInt("cantidad");
-                String nombreMarca = resultSet.getString("nombreMarca");
+                String nombreMarca = resultSet.getString("marca");
 
                 Bacha bacha = new Bacha(marcasBachasID, nombreModelo, medidas, cantidad);
+                bacha.setBachasID(bachasID);
+                bacha.setTipoBacha(tipoBacha);
                 bacha.setNombreMarca(nombreMarca);
+
                 bachasList.add(bacha);
             }
 
-        } catch (Exception e) {
+        } catch (SQLException e) {
             throw new SQLException("Error al buscar bachas: " + e.getMessage(), e);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
         }
 
         return bachasList;
